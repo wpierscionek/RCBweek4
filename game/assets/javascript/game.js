e184138c230c539d8bb5cdb2fff82a243c09c88c
Wojciech Pierscionek <PierscionekW@gmail.com>
2016-02-22 00:42:23 -0400

    updated week 4

---
+++ game/assets/javascript/game.js
@@ -0,0 +1,21 @@
+// GLOBAL VARIABLES ACCESSIBLE BY ALL FUNCTSION
+$(document).ready(function() {
+      alert("document ready occurred!");
+});
+
+	$("#yellowStone").on('click' , function(){
+		alert('hi');
+	});
+
+	$("#diamondStone").on('click' , function(){
+		alert('hi');
+	});
+
+	$("#blueStone").on('click' , function(){
+		alert('hi');
+	});
+
+	$("#rubyStone").on('click' , function(){
+		alert('hi');
+	});
+});
