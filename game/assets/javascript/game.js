9631ba646706499abebf7805c3b71664363febb5
Wojciech Pierscionek <PierscionekW@gmail.com>
2016-02-25 13:32:24 -0400

    complete

--- game/assets/javascript/game.js
+++ game/assets/javascript/game.js
@@ -1,21 +1,87 @@
-// GLOBAL VARIABLES ACCESSIBLE BY ALL FUNCTSION
-$(document).ready(function() {
-      alert("document ready occurred!");
-});
-
-	$("#yellowStone").on('click' , function(){
-		alert('hi');
-	});
-
-	$("#diamondStone").on('click' , function(){
-		alert('hi');
-	});
-
-	$("#blueStone").on('click' , function(){
-		alert('hi');
-	});
-
-	$("#rubyStone").on('click' , function(){
-		alert('hi');
-	});
-});
+$( document ).ready(function() {
+    // console.log( "ready!" );
+		var numberList = [20,22,30,40,46,49,50,58,62,68,64,69,82,91,93,110,115,120]; 
+		var chosenNumber = 0;
+		// game counters
+		var winCounter = 0;
+		var looseCounter = 0;
+		var score = 0;
+		// Audio
+		var click = new Audio("click.wav");
+		var victory = new Audio("victory.wav");
+		var wrong = new Audio("wrong.wav");
+		// Stone values
+		var yellowStone = Math.floor((Math.random() * 10) +1);
+		var diamondStone = Math.floor((Math.random() * 10) +1);
+		var blueStone = Math.floor((Math.random() * 10) +1);
+		var rubyStone = Math.floor((Math.random() * 10) +1);
+		// console.log(yellowStone, diamondStone, blueStone, rubyStone);
+		function updateScore() {
+			$(".randomNumber").empty();
+			$(".randomNumber").append(chosenNumber);
+			$(".win").empty();
+			$(".win").append(winCounter);
+			$(".loose").empty();
+			$(".loose").append(looseCounter);
+			$(".score").empty();
+			$(".score").append(score);
+		}
+		function startGame() {
+			winCounter = 0;
+			looseCounter = 0;
+			score = 0;
+			chosenNumber = numberList[Math.floor(Math.random() * numberList.length)];
+		}
+		function restart() {
+			score = 0;
+			chosenNumber = numberList[Math.floor(Math.random() * numberList.length)];
+			yellowStone = Math.floor((Math.random() * 10) +1);
+			diamondStone = Math.floor((Math.random() * 10) +1);
+			blueStone = Math.floor((Math.random() * 10) +1);
+			rubyStone = Math.floor((Math.random() * 10) +1);
+			updateScore();
+		}
+		var logic = function () {
+			if(score == chosenNumber) {
+			winCounter++;
+			$(".win").html = winCounter; 
+			victory.play();
+			restart();
+		}
+			else if( score > chosenNumber) {
+			looseCounter++;
+			$("#loose").html = looseCounter; 
+			wrong.play();
+			restart();
+			
+		}
+			else {
+				updateScore();
+		}
+		}
+			$("#yellowStone").click(function(){
+				 click.play();
+				 click.currentTime = 0;
+				 score = score + yellowStone;
+				 logic();
+			});
+			$("#diamondStone").click(function(){
+				 click.play();
+				 click.currentTime = 0;
+				 score = score + diamondStone;
+				 logic();
+			});
+			$("#blueStone").click(function(){
+				 click.play();
+				 click.currentTime = 0;
+				 score = score + blueStone;
+				 logic();
+			});
+			$("#rubyStone").click(function(){
+				click.play();
+				click.currentTime = 0;
+				score = score + rubyStone;
+				logic();
+			});
+			startGame();
+		});
